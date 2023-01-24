ebd2c5a99a5157e39f283127f779d919cb7ba78e
Alka <123442887+Alka-Raveendran@users.noreply.github.com>
2023-01-24 14:28:06 +0530

    Update Covid19-Data-Exploration-In-SQL.sql

--- Covid19-Data-Exploration-In-SQL.sql
+++ Covid19-Data-Exploration-In-SQL.sql
@@ -1,26 +1,41 @@
+
+
+
 -- Use the Database portfolio --
 USE portfolio;
+
+
 
 -- Select everythiong from the first table - Coviddeaths --
 SELECT * FROM coviddeaths;
 
+
+
 -- Select everythiong from the Second Table - covidvaccinations --
 SELECT * FROM covidvaccinations;
+
+
 
 -- Select data that we are using for the data analysis from Coviddeaths’s table --
 SELECT location, date, population, total_cases, new_cases, total_deaths, new_deaths
 FROM coviddeaths
 ORDER BY 1,2;
 
+
+
 -- Shows how Covid19 evolved by time --
 SELECT date, location, new_cases, new_deaths, (new_deaths/new_cases)*100 AS PercentageCasesOverTime
 FROM coviddeaths
 ORDER BY 1;
 
+
+
 -- Total cases by total deaths --
 SELECT location, date, population, total_cases, total_deaths, (total_deaths/total_cases)*100 AS PercentageDeathRate
 FROM coviddeaths
 ORDER BY 1,2;
+
+
 
 -- Total cases Vs population --
 -- Shows what percent of population got Covid --
@@ -28,11 +43,15 @@ SELECT location, date, population,(total_cases/population)*100 AS PercentagePopu
 FROM coviddeaths
 ORDER BY 1,2;
 
+
+
 -- Total death Vs population --
 -- Shows what percent of population died due to Covid --
 SELECT location, date, population,(total_deaths/population)*100 AS PercentagePopulationDied
 FROM coviddeaths
 ORDER BY 1,2;
+
+
 
 -- Top 10 countries with highest infection rate compared to the population --
 SELECT location, population, MAX(total_cases) AS MaxInfectionCount,
@@ -42,6 +61,8 @@ GROUP BY location
 ORDER BY MaxPercentagePopulationInfected DESC
 LIMIT 10;
 
+
+
 -- Top 10 countries with highest death Rate compared to the population --
 SELECT location, population, MAX(total_deaths) AS MaxDeathCount,
 MAX((total_deaths/population)*100) AS MaxPercentageDeathPerPopulation
@@ -49,6 +70,8 @@ FROM coviddeaths
 GROUP BY location
 ORDER BY MaxPercentageDeathPerPopulation DESC
 LIMIT 10;
+
+
 
 -- Global Count --
 -- Continents with the highest death count based on the population --
@@ -59,10 +82,13 @@ WHERE continent IS NOT NULL
 GROUP BY continent
 ORDER BY MaxDeathPercentagePerPopulationByContinent DESC;
 
+
+
 -- Global count of total cases and total deaths across the world --
 SELECT SUM(new_cases) AS GlobalCovid19Cases, SUM(new_deaths) AS GlobalCovid19Deaths, (SUM(new_deaths)/SUM(new_cases)) AS GlobalDeathPercentage
 FROM coviddeaths
 WHERE continent IS NOT NULL;
+
 
 
 -- Select data that we are using for the data analysis from Covidvaccinations’s table --
@@ -70,10 +96,14 @@ SELECT location, date, new_tests, total_vaccinations, people_vaccinated, new_vac
 FROM covidvaccinations
 ORDER BY 1,2;
 
+
+
 -- Showing all the details about Covid cases, deaths and vaccinations --
 SELECT * FROM coviddeaths dea
 JOIN covidvaccinations vac 
 WHERE dea.date = vac.date AND dea.location = vac.location;
+
+
 
 -- Looking at the accumulation of vaccination based on the date and location --
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
